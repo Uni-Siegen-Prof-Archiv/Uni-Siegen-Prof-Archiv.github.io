8dac85e7fb0f485aab590ddf2ec5530b205465eb
Thore <the_unknown_max_mustermann@proton.me>
2025-01-19 19:27:41 +0100

    remove duplicated id reference

--- warning-screen.js
+++ warning-screen.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const warnScreen = document.getElementById('warnScreen');
+    const mainContent = document.getElementById('mainContent');
+
+    warnScreen.style.display = 'flex';
+    mainContent.style.display = 'none';
+
     setTimeout(() => {
-        const warnScreen = document.getElementById('warnScreen');
-        const mainContent = document.getElementById('mainContent');
-
         warnScreen.style.opacity = '0';
-        warnScreen.style.transition = 'opacity 1s';
+        warnScreen.style.transition = 'opacity 1s ease-in-out';
 
-        mainContent.style.display = 'flex';
+        mainContent.style.display = 'block';
 
         setTimeout(() => {
             warnScreen.style.display = 'none';
+            document.body.style.overflow = 'auto';
         }, 1000);
-    }, 2000);
+    }, 1500);
 });
